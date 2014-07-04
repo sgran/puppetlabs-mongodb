34fa6baf5a7816e8101336bb326a2eeff7ffcd37
Stephen Gran <steve@lobefin.net>
2014-07-04 07:25:09 +0100

    needed better stubbing
    Signed-off-by: Stephen Gran <steve@lobefin.net>

--- spec/unit/puppet/provider/mongodb_user/mongodb_spec.rb
+++ spec/unit/puppet/provider/mongodb_user/mongodb_spec.rb
@@ -14,9 +14,17 @@ describe Puppet::Type.type(:mongodb_user).provider(:mongodb) do
 
   let(:provider) { resource.provider }
 
+  describe 'version' do
+    it 'returns a version' do
+      provider.expects(:mongo).at_least(1).returns('MongoDB shell version: 2.6.0')
+      provider.version.should == 260
+    end
+  end
+
   describe 'create' do
     it 'creates a user' do
       provider.expects(:mongo)
+      provider.stubs(:version).returns(260)
       provider.create
     end
   end
@@ -24,6 +32,7 @@ describe Puppet::Type.type(:mongodb_user).provider(:mongodb) do
   describe 'destroy' do
     it 'removes a user' do
       provider.expects(:mongo)
+      provider.stubs(:version).returns(260)
       provider.destroy
     end
   end
@@ -31,6 +40,7 @@ describe Puppet::Type.type(:mongodb_user).provider(:mongodb) do
   describe 'exists?' do
     it 'checks if user exists' do
       provider.expects(:mongo).at_least(2).returns("1")
+      provider.stubs(:version).returns(260)
       provider.exists?.should eql true
     end
   end
@@ -38,6 +48,7 @@ describe Puppet::Type.type(:mongodb_user).provider(:mongodb) do
   describe 'password_hash' do
     it 'returns a password_hash' do
       provider.expects(:mongo).returns("pass\n")
+      provider.stubs(:version).returns(260)
       provider.password_hash.should == "pass"
     end
   end
@@ -45,6 +56,7 @@ describe Puppet::Type.type(:mongodb_user).provider(:mongodb) do
   describe 'password_hash=' do
     it 'changes a password_hash' do
       provider.expects(:mongo)
+      provider.stubs(:version).returns(260)
       provider.password_hash=("newpass")
     end
   end
@@ -52,6 +64,7 @@ describe Puppet::Type.type(:mongodb_user).provider(:mongodb) do
   describe 'roles' do
     it 'returns a sorted roles' do
       provider.expects(:mongo).returns("role2,role1\n")
+      provider.stubs(:version).returns(260)
       provider.roles.should == ['role1','role2']
     end
   end
@@ -59,6 +72,7 @@ describe Puppet::Type.type(:mongodb_user).provider(:mongodb) do
   describe 'roles=' do
     it 'changes a roles' do
       provider.expects(:mongo)
+      provider.stubs(:version).returns(260)
       provider.roles=(['role3','role4'])
     end
   end
