e0dbbcf50540fa15032a323997fdb66ab8298552
Stephen Gran <steve@lobefin.net>
2014-07-04 07:30:56 +0100

    off by one error
    Signed-off-by: Stephen Gran <steve@lobefin.net>

--- lib/puppet/provider/mongodb_user/mongodb.rb
+++ lib/puppet/provider/mongodb_user/mongodb.rb
@@ -22,26 +22,26 @@ Puppet::Type.type(:mongodb_user).provide(:mongodb) do
   end
 
   def create
-    if version > 260
+    if version >= 260
       mongo(@resource[:database], '--eval', "db.createUser({user:\"#{@resource[:name]}\", pwd:\"#{@resource[:password]}\", roles: #{@resource[:roles].inspect}})")
-    elsif version > 240
+    elsif version >= 240
       mongo(@resource[:database], '--eval', "db.system.users.insert({user:\"#{@resource[:name]}\", pwd:\"#{@resource[:password_hash]}\", roles: #{@resource[:roles].inspect}})")
     end
   end
 
   def destroy
-    if version > 260
+    if version >= 260
       mongo(@resource[:database], '--quiet', '--eval', "db.dropUser(\"#{@resource[:name]}\")")
-    elsif version > 240
+    elsif version >= 240
       mongo(@resource[:database], '--quiet', '--eval', "db.removeUser(\"#{@resource[:name]}\")")
     end
   end
 
   def exists?
     block_until_mongodb(@resource[:tries])
-    if version > 260
+    if version >= 260
       mongo('admin', '--quiet', '--eval', "db.system.users.find({user:\"#{@resource[:name]}\"}).count()").strip.eql?('1')
-    elsif version > 240
+    elsif version >= 240
       mongo(@resource[:database], '--quiet', '--eval', "db.system.users.find({user:\"#{@resource[:name]}\"}).count()").strip.eql?('1')
     end
   end
@@ -59,34 +59,34 @@ Puppet::Type.type(:mongodb_user).provide(:mongodb) do
   end
 
   def password_hash
-    if version > 260
+    if version >= 260
       mongo('admin', '--quiet', '--eval', "db.system.users.findOne({user:\"#{@resource[:name]}\"})[\"credentials\"][\"MONGODB-CR\"]").strip
-    elsif version > 240
+    elsif version >= 240
       mongo(@resource[:database], '--quiet', '--eval', "db.system.users.findOne({user:\"#{@resource[:name]}\"})[\"pwd\"]").strip
     end
   end
 
   def password_hash=(value)
-    if version > 260
+    if version >= 260
       mongo('admin', '--quiet', '--eval', "db.system.users.update({user:\"#{@resource[:name]}\"}, { $set: {pwd:\"#{value}\"}})")
-    elsif version > 240
+    elsif version >= 240
       mongo(@resource[:database], '--quiet', '--eval', "db.system.users.update({user:\"#{@resource[:name]}\"}, { $set: {pwd:\"#{value}\"}})")
     end
   end
 
   def roles
-    if version > 260
+    if version >= 260
       JSON.load(mongo('admin', '--quiet', '--eval', "printjson(db.system.users.findOne({user:\"#{@resource[:name]}\"})[\"roles\"])").strip).collect{ |x| x['role'] if x['db'] == @resource[:database] }
-    elsif version > 240
+    elsif version >= 240
       mongo(@resource[:database], '--quiet', '--eval', "db.system.users.findOne({user:\"#{@resource[:name]}\"})[\"roles\"]").strip.split(",").sort
     end
   end
 
   def roles=(value)
-    if version > 260
+    if version >= 260
       roles = JSON.load(@resource[:roles].inspect).map{ |x| {"db" => @resource[:database], "role" => x} }.to_json()
       mongo('admin', '--quiet', '--eval', "db.updateUser(\"#{@resource[:name]}\", { roles: #{roles}})")
-    elsif version > 240
+    elsif version >= 240
       mongo(@resource[:database], '--quiet', '--eval', "db.system.users.update({user:\"#{@resource[:name]}\"}, { $set: {roles: #{@resource[:roles].inspect}}})")
     end
   end
--- spec/unit/puppet/provider/mongodb_user/mongodb_spec.rb
+++ spec/unit/puppet/provider/mongodb_user/mongodb_spec.rb
@@ -24,7 +24,7 @@ describe Puppet::Type.type(:mongodb_user).provider(:mongodb) do
   describe 'create' do
     it 'creates a user' do
       provider.expects(:mongo)
-      provider.stubs(:version).returns(260)
+      provider.stubs(:version).returns(248)
       provider.create
     end
   end
@@ -32,7 +32,7 @@ describe Puppet::Type.type(:mongodb_user).provider(:mongodb) do
   describe 'destroy' do
     it 'removes a user' do
       provider.expects(:mongo)
-      provider.stubs(:version).returns(260)
+      provider.stubs(:version).returns(248)
       provider.destroy
     end
   end
@@ -40,7 +40,7 @@ describe Puppet::Type.type(:mongodb_user).provider(:mongodb) do
   describe 'exists?' do
     it 'checks if user exists' do
       provider.expects(:mongo).at_least(2).returns("1")
-      provider.stubs(:version).returns(260)
+      provider.stubs(:version).returns(248)
       provider.exists?.should eql true
     end
   end
@@ -48,7 +48,7 @@ describe Puppet::Type.type(:mongodb_user).provider(:mongodb) do
   describe 'password_hash' do
     it 'returns a password_hash' do
       provider.expects(:mongo).returns("pass\n")
-      provider.stubs(:version).returns(260)
+      provider.stubs(:version).returns(248)
       provider.password_hash.should == "pass"
     end
   end
@@ -56,7 +56,7 @@ describe Puppet::Type.type(:mongodb_user).provider(:mongodb) do
   describe 'password_hash=' do
     it 'changes a password_hash' do
       provider.expects(:mongo)
-      provider.stubs(:version).returns(260)
+      provider.stubs(:version).returns(248)
       provider.password_hash=("newpass")
     end
   end
@@ -64,7 +64,7 @@ describe Puppet::Type.type(:mongodb_user).provider(:mongodb) do
   describe 'roles' do
     it 'returns a sorted roles' do
       provider.expects(:mongo).returns("role2,role1\n")
-      provider.stubs(:version).returns(260)
+      provider.stubs(:version).returns(248)
       provider.roles.should == ['role1','role2']
     end
   end
@@ -72,7 +72,7 @@ describe Puppet::Type.type(:mongodb_user).provider(:mongodb) do
   describe 'roles=' do
     it 'changes a roles' do
       provider.expects(:mongo)
-      provider.stubs(:version).returns(260)
+      provider.stubs(:version).returns(248)
       provider.roles=(['role3','role4'])
     end
   end
